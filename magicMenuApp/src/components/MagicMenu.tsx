98cf49e1bc2a51ad24b0a131fd0fd199810a11c3
Curro Jimenez <curro.jimenez@grapheverywhere.com>
2020-10-09 17:30:01 +0200

    First Commit

---
+++ magicMenuApp/src/components/MagicMenu.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent, useState } from 'react';
+
+
+interface Props {
+};
+
+
+const MagicMenu: FunctionComponent<Props> = (props: Props) => {
+
+    return (
+        <div>
+
+        </div>
+        
+    );
+}
+
+export default MagicMenu
